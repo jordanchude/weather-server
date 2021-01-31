665bb5d3fb6a57f718b7743bb9ba10100f2d2598
Jordan <jordan@Jordans-MacBook-Pro.local>
2021-01-31 15:53:38 -0500

    first handlebars implementation

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 // DEPENDENCIES
+const path = require('path');
 const express = require('express');
+const { readdirSync } = require('fs');
 
-console.log(__dirname)
-console.log(__filename)
-
-// used to store express function
+//load instance of express
 const app = express()
+
+
+//generate path to public folder
+const publicDirectoryPath = path.join(__dirname, '../public')
+const helpPath = path.join(__dirname, '../public/help.html')
+const aboutPath = path.join(__dirname, '../public/about.html')
+
+// tell node which templating engine to use when installed
+// need this line to set up handlebars
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
+// app.use('/help', express.static(helpPath))
+// app.use('/about', express.static(aboutPath))
 
 // configures what the server should do when someone tries to get the resource at a url
 // ex. HTML or JSON
@@ -14,22 +26,27 @@ const app = express()
 
 // ROUTES
 app.get('', (req, res) => {
-    // allows to send something back to requester
-    res.send('<h1>Weather</h1>');
+    // render one of our views
+    // first argument is name of view we want to use, second argument is an object
+    // no need for file extension; just needs name of template
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
 })
 
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Jordan',
-        age: 27
-    }, {
-        name: 'Emily',
-        age: 26
-    }]);
-});
-
 app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>')
+    res.render('about', {
+        title: 'About me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/message', (req, res) => {
+    res.render('help', {
+        title: "Help",
+        message: "This is a test help message."
+    })
 })
 
 app.get('/weather', (req, res) => {
